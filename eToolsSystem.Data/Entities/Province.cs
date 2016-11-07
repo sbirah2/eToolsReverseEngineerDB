f409c7bd9c54997ae83637a15dbe8dc1252354b9
sbirah2 <sbirah2@studentmail.nait.ca>
2016-11-07 16:03:21 -0700

    First Commit

---
+++ eToolsSystem.Data/Entities/Province.cs
@@ -0,0 +1,27 @@
+namespace eToolsSystem.Data.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Province
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Province()
+        {
+            Vendors = new HashSet<Vendor>();
+        }
+
+        [StringLength(2)]
+        public string ProvinceID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Vendor> Vendors { get; set; }
+    }
+}
---
+++ eToolsSystem.Data/Entities/SaleDetail.cs
@@ -0,0 +1,30 @@
+namespace eToolsSystem.Data.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class SaleDetail
+    {
+        public int SaleDetailID { get; set; }
+
+        public int SaleID { get; set; }
+
+        public int StockItemID { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal SellingPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool Backordered { get; set; }
+
+        public DateTime? ShippedDate { get; set; }
+
+        public virtual Sale Sale { get; set; }
+
+        public virtual StockItem StockItem { get; set; }
+    }
+}
